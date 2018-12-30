9bc1fc7fe5e733fee20b4d8825a5494284bd31cd
Fco Javier Núñez Berrocoso <javiernuber@gmail.com>
2018-12-30 11:53:45 +0100

    Base webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = (env) => {
+  let mode = 'development';
+  const plugins = [
+    new MiniCssExtractPlugin({filename: "css/[name].css"}),   
+  ]
+
+  if (env && env.NODE_ENV === 'production') {
+    plugins.push(
+      new CleanWebpackPlugin(['dist'], {root: __dirname})
+    );
+    mode = 'production'
+  }
+
+  return {
+    mode: mode,
+    entry: {
+      app: path.resolve(__dirname, 'src/index.js'),
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'js/[name].js',
+      publicPath: path.resolve(__dirname, 'dist')+"/",
+      chunkFilename: 'js/[id].[chunkhash].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            }
+          },
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: '../'
+              }
+            },
+            "css-loader"
+          ]
+        },
+        {
+          test: /\.(jpg|png|gif|svg)$/,
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              fallback: 'file-loader',
+              name: 'images/[name].[hash].[ext]',
+            }
+          }
+        },
+      ]
+    },
+    plugins
+  }
+}
