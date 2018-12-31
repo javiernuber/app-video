ed06f45d7e599214cc117586cef6c84b74532a90
Fco Javier Núñez Berrocoso <javiernuber@gmail.com>
2018-12-31 11:55:36 +0100

    ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+
 console.log('Hello word');
+
+const app = document.getElementById('app');
+const content = <h1>Hello world!!!!!!!</h1>;
+
+ReactDOM.render(content, app);
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: {
-    invie: path.resolve(__dirname, 'src/index.js'),
+    app: path.resolve(__dirname, 'src/index.js'),
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
